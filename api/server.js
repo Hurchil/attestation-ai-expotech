55db47d33f605a0e50c865a997df8ea499c1d88f
hurchil <tchindadjate@gmail.com>
2026-03-19 07:20:04 +0100

    autorise tout

--- api/server.js
+++ api/server.js
@@ -14,11 +14,13 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin: ["https://admin-ai-expotech.netlify.app/", "https://attestation-ai-expotech.netlify.app/","*"], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// app.use(cors({
+//   origin: ["https://admin-ai-expotech.netlify.app/", "https://attestation-ai-expotech.netlify.app/","*"], 
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+app.use(cors()); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
